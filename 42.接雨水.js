70782cbaf643de6810a0a0a4933a23538afea0a4
Carl, Chen <ayang879@gmail.com>
2020-02-27 20:52:42 +0800

    42 finished

--- 42.接雨水.js
+++ 42.接雨水.js
@@ -9,39 +9,82 @@
  * @param {number[]} height
  * @return {number}
  */
-var trap = function(heights) {
-	function calc(arr) {
-		if (arr.length < 3) {
+var trap = function(height) {
+	function calc(start, end) {
+		if (end - start < 2) {
 			return 0;
 		}
 		let temp = 0,
-			base = Math.min(arr[0], arr[arr.length - 1]),
-			index = 1;
-		while (index < arr.length - 1) {
-			temp += base - arr[index];
+			base = Math.min(height[start], height[end]),
+			index = start + 1;
+		while (index < end) {
+			temp += base - height[index];
+			index++;
 		}
 		return temp;
 	}
 
-	function rec(arr) {
-		let highest = Math.max.apply(null, arr);
+	function lastIndex(start, end, target) {
+		let index = end;
+		while (index > start - 1) {
+			if (height[index] === target) {
+				return index;
+			}
+			index--;
+		}
+		return -1;
+	}
+
+	function firstIndex(start, end, target) {
+		let index = start;
+		while (index < end + 1) {
+			if (height[index] === target) {
+				return index;
+			}
+			index++;
+		}
+		return -1;
+	}
+
+	function max(start, end) {
+		let max = -Infinity;
+		for (let index = start; index < end + 1; index++) {
+			if (height[index] > max) {
+				max = height[index];
+			}
+		}
+		return max;
+	}
+
+	function rec(start, end) {
+		if (end - start < 2) {
+			return 0;
+		}
+		let highest = max(start, end);
 		let temp = 0;
-		let leftArr, rightArr;
-		temp += calc(arr.slice([arr.indexOf(highest)], arr.lastIndexOf[highest] + 1));
-
-		
-
-
-		leftArr = arr.slice(0, arr.indexOf(highest));
-		rightArr = arr.slice(arr.lastIndexOf(highest) + 1);
-		if (highestIndexes[0] !== 0) {
-			temp += trap(heights.slice(0, highestIndexes[0]));
-		}
-		if (highestIndexes[highestIndexes.length - 1] !== heights.length - 1) {
-			temp += trap(heights.slice(highestIndexes[highestIndexes.length - 1]));
+		let last = lastIndex(start, end, highest),
+			first = firstIndex(start, end, highest);
+		if (last != first) {
+			temp += calc(first, last);
+			temp += rec(start, first);
+			temp += rec(last, end);
+		} else if (first == end) {
+			let second = max(start, end - 1);
+			let secondIndex = firstIndex(start, end - 1, second);
+			temp += calc(secondIndex, end);
+			temp += rec(start, secondIndex);
+		} else if (last == start) {
+			let second = max(start + 1, end);
+			let secondIndex = lastIndex(start + 1, end, second);
+			temp += calc(start, secondIndex);
+			temp += rec(secondIndex, end);
+		} else {
+			temp += rec(first, end);
+			temp += rec(start, first);
 		}
 		return temp;
 	}
+	return rec(0, height.length - 1);
 };
-trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]);
+// trap([2, 0, 2]);
 // @lc code=end
